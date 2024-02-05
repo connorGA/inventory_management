4d754cce053859aec3726ec12278c4dce76b2f78
connorGA <ronnoccap@gmail.com>
2024-02-05 15:45:57 -0800

    added update functionality

--- inventory_management/inventory/views.py
+++ inventory_management/inventory/views.py
@@ -1,7 +1,7 @@
 from typing import Any
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import TemplateView, View, CreateView
+from django.views.generic import TemplateView, View, CreateView, UpdateView
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .forms import UserRegisterForm, InventoryItemForm
@@ -49,3 +49,10 @@ class AddItem(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+class EditItem(LoginRequiredMixin, UpdateView):
+     model = InventoryItem
+     form_class= InventoryItemForm
+     template_name = 'inventory/item_form.html'
+     success_url = reverse_lazy('dashboard')
+    
